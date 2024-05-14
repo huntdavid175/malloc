927b598e87f5fdb569c1c7fd209990b5533d9ef7
huntdavid175 <huntdavid175@gmail.com>
2024-05-14 19:34:00 -0400

    created the fillFlightInfo function

--- focus1.c
+++ focus1.c
@@ -16,3 +16,29 @@ int main(void)
 {
     return 0;
 }
+
+void fillFlightInfo(struct Flight *flight, char destination[], char date[])
+{
+    size_t destinationLength = strlen(destination);
+    size_t dateLength = strlen(date);
+
+    flight->destination = (char *)malloc(destinationLength + 1);
+
+    if (flight->destination == NULL)
+    {
+        printf("Memory not available");
+        return;
+    }
+
+    strcpy(flight->destination, destination);
+
+    flight->date = (char *)malloc(dateLength + 1);
+
+    if (flight->date == NULL)
+    {
+        printf("Memory not availble");
+        return;
+    }
+
+    strcpy(flight->date, date);
+}
